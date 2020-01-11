1a9d81fd36e109ce9132ce9a202293ec29b45197
cyj199637 <cyj199637@gmail.com>
2020-01-12 01:58:58 +0900

    200109| 북스토어 애플리케이션 MySQL과 연동

--- Django/01_bookstore/bookstore/views.py
+++ Django/01_bookstore/bookstore/views.py
@@ -1,16 +1,60 @@
 from django.shortcuts import render, get_object_or_404
+from django.db import connection
 from .models import Book
 
 # Create your views here.
 def BookListView(request):
-    books = Book.objects.all()
+    # books = Book.objects.all()
+    try:
+        cursor = connection.cursor()
+
+        strSql = "SELECT code, name, author FROM bookstore_book"
+        result = cursor.execute(strSql)
+        datas = cursor.fetchall()
+
+        connection.commit()
+        connection.close()
+
+        books = []
+        for data in datas:
+            row = {'code': data[0],
+                   'name': data[1],
+                   'author': data[2]}
+
+            books.append(row)
+
+    except:
+        connection.rollback()
+        print("Failed selecting in BookListView")
+
 
     return render(request, 'book_list.html', {'books': books})
 
 
 
 def BookDetailView(request, code):
-    book1 = get_object_or_404(Book, code=code)
-    book2 = Book.objects.get(code=code)
+    # book1 = get_object_or_404(Book, code=code)
+    # book2 = Book.objects.get(code=code)
 
-    return render(request, 'book_detail.html', {'book': book2})
+    try:
+        cursor = connection.cursor()
+
+        strSql = "SELECT code, name, author, price, url FROM bookstore_book WHERE code = (%s)"
+        result = cursor.execute(strSql, (code,))
+        datas = cursor.fetchall()
+
+        connection.commit()
+        connection.close()
+
+        book = {'code': datas[0][0],
+                'name': datas[0][1],
+                'author': datas[0][2],
+                'price': datas[0][3],
+                'url': datas[0][4]}
+
+    except:
+        connection.rollback()
+        print("Failed selecting in BookListView")
+
+
+    return render(request, 'book_detail.html', {'book': book})
--- Django/01_bookstore/mysite/settings.py
+++ Django/01_bookstore/mysite/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# MySQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf')
+        },
     }
 }
+
 
 
 # Password validation
